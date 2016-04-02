96b87af94d166b8094771f05787c41f885118983
shivangag <shivang1997agrawal@gmail.com>
2016-04-02 18:53:37 +0530

    Cleaned icare

--- src/pathplanner.cpp
+++ src/pathplanner.cpp
@@ -1,27 +1,19 @@
 #include <stdlib.h>
 #include "ros/ros.h"
-#include "std_msgs/Float32.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/Point.h"
+#include "geometry_msgs/Point32.h"
+#include "icare/Steer.h"
 #define MAX_DISTANCE 200
 #define SAFE_DISTANCE 50
-#define move 15
-#define obstacle 30
+#define MOVE 15
+#define SPEED 85
+#define STOP 0
 
-//volatile float leftDistance,rightDistance,straightDistance;
 float sonar[3] = {0,0,0};
 float yaw = 0;
-float handle_angle = 0;
-std_msgs::Float32 angle_msg;
-ros::Subscriber sonarSub;
-ros::Publisher handleAnglePub;
-void settings(void);
-void Heading(void);
-void changeDIR(int);
-void Update(float);
-void Brake(void);
 
-void sonarCallback(const geometry_msgs::Point::ConstPtr& msg){
+
+
+void sonarCallback(const geometry_msgs::Point32::ConstPtr& msg){
 	sonar[0] = msg->x;
 	sonar[1] = msg->y;
 	sonar[2] = msg->z;
@@ -29,98 +21,35 @@ void sonarCallback(const geometry_msgs::Point::ConstPtr& msg){
 }
 
 int main(int argc,char** argv){
-	float count=0;
 	ros::init(argc,argv,"pathplanner");
+	icare::Steer steer_msg;
 	ros::NodeHandle n;
 
-	handleAnglePub = n.advertise<std_msgs::Float32>("Handle_angle",50);
-	ros::Rate loop_rate(100);
+	ros::Publisher steerPub = n.advertise<icare::Steer>("Steer",200);
+	ros::Rate loop_rate(10);
 	
 
-	sonarSub = n.subscribe("sonar",50,sonarCallback);
+	ros::Subscriber sonarSub = n.subscribe("sonar",200,sonarCallback);
+
 	while(ros::ok()){
+		steer_msg.angle=0;
 		if(sonar[1]<SAFE_DISTANCE){
 			if(sonar[0]>SAFE_DISTANCE||sonar[2]>SAFE_DISTANCE){
+				steer_msg.speed=SPEED;
 				if(sonar[0]>sonar[1])
 					//directionMsg.data = "left";
-					angle_msg.data=-1;
+					steer_msg.angle=MOVE;
 				else
-					angle_msg.data=1;
+					steer_msg.angle=-1*MOVE;
 			}
 			else
-				angle_msg.data=-99;
+				steer_msg.speed=STOP;
 		}
 		else
-			angle_msg.data=100;
-		handleAnglePub.publish( angle_msg );
+			steer_msg.speed=SPEED;
+		steerPub.publish( steer_msg );
 		loop_rate.sleep();
 		ros::spinOnce();
-		//Heading();
-		//angle_msg.data=count++;
-		//handleAnglePub.publish(angle_msg);
-		//ros::spinOnce();
-		//loop_rate.sleep();
-		//Heading();
-		//ros::spinOnce();
 	}
 	
-}
-
-
-void Heading()
-{
-
-	if(sonar[0] < obstacle && sonar[1] < obstacle && sonar[2] < obstacle)
-		{Brake();
-			return;}
-	if(sonar[0]<sonar[1]&&sonar[0]<sonar[2])
-		{changeDIR(1);return;}
-	else if(sonar[1]<sonar[0]&&sonar[1]<sonar[2])
-		{changeDIR(2);
-			return;}
-	else if(sonar[2]<sonar[1]&&sonar[2]<sonar[0])
-		{changeDIR(3);
-			return;}
-}
-void changeDIR(int Obstructed_sonar)
-{
- float angle = 0;
-	switch(Obstructed_sonar)
-		{
-			case 1:
-				if(sonar[1] > sonar[2])
-					angle-=move;
-				else
-					angle-=(2*move);
-			case 2:
-				if(sonar[0] > sonar[2])
-					angle+=move;
-				else
-					angle-=move;
-			case 3:
-				if(sonar[1] > sonar[0])
-					angle+=move;
-				else
-					angle+=(2*move);
-		}
-	Update(angle);
-}
-void Update(float angle)
-{
-	handle_angle = angle;
-	angle_msg.data = handle_angle;
-	handleAnglePub.publish(angle_msg);
-	while(sonar[0] < obstacle && sonar[1] < obstacle && sonar[2] < obstacle){
-		ros::spinOnce();
-	}
-	handle_angle = 0;
-	angle_msg.data = handle_angle;
-	handleAnglePub.publish(angle_msg);	
-}
-void Brake()
-{
-	//braking 
-	angle_msg.data = -999;
-	handleAnglePub.publish(angle_msg);
-
 }
